be0ca67eb06612a3f4f1fe55490fe5ee4470c720
Jeremy Boggs <jeremy@clioweb.org>
2014-08-22 10:19:05 -0400

    Updates path to file2brl.

--- app.rb
+++ app.rb
@@ -31,64 +31,64 @@ end
 
 ##User is posting to the /braille endpoint
 post '/braille' do
-	
+
 	##Create the input and output files for the system call
 	input_file = Tempfile.new('braille')
 	output_file = Tempfile.new('braille')
-	
+
 	##Write the message that the caller provided in the message param to the input_file var
 	input_file.write("#{params[:content]}")
-	
-	##If the input file has content, then proceed with the translation 
+
+	##If the input file has content, then proceed with the translation
 	## otherwise, exit with status code 501 "You must specify content to convert to braille."
 	if input_file.size > 0
-	
-		begin 
-		
-			##Make the system call 
-			system("file2brl -p #{input_file.path} #{output_file.path}")
-		
+
+		begin
+
+			##Make the system call
+			system("./bin/file2brl -p #{input_file.path} #{output_file.path}")
+
 			##Check to make sure that the length of the output file is greater than 0; if not, then
-			## exit with status 502 "Content not successfully converted to braille." 
-	
+			## exit with status 502 "Content not successfully converted to braille."
+
 			if output_file.size > 0
-				
+
 				##Return the output file contents
 				"#{output_file.read}"
-				
+
 			else
-			
+
 				##Exit with status code 502
 				status 502
-				"Content not successfully converted to Braille." 
-				
+				"Content not successfully converted to Braille."
+
 			end
-	
+
 		ensure
-	
+
 			##Close the files
 			input_file.close
 			output_file.close
-	
+
 			##Unlink the files
 			input_file.unlink
 			output_file.unlink
-			
+
 		end
-		
+
 	else
-		
+
 		##Close the files
 		input_file.close
 		output_file.close
-	
+
 		##Unlink the files
 		input_file.unlink
 		output_file.unlink
-		
+
 		status 400
 		"You must specify content to convert to Braille."
-			
+
 	end
 
 end
@@ -98,64 +98,64 @@ post '/braille.json' do
 ##Create the input and output files for the system call
 	input_file = Tempfile.new('braille')
 	output_file = Tempfile.new('braille')
-	
+
 	request.body.rewind  # in case someone already read it
  	 data = JSON.parse request.body.read
-	
+
 	##Write the message that the caller provided in the message param to the input_file var
 	input_file.write("#{data['content']}")
-	
+
 	##If the input file has content, then proceed with the translation
 	## otherwise, exit with the status code 501 "You must specify content to convert to braille."
 	if input_file.size > 0
-	
+
 		begin
-			
+
 			##Make the system call
-			system("file2brl -p #{input_file.path} #{output_file.path}")
-			
-			##Check to make sure that the length of the output file is great than 0; if not, then 
+			system("./bin/file2brl -p #{input_file.path} #{output_file.path}")
+
+			##Check to make sure that the length of the output file is great than 0; if not, then
 			## exit with a status 502 "Content not successfully converted to braille."
-			
-			if output_file.size > 0	
-			
+
+			if output_file.size > 0
+
 				## Return the output file contents
 				outputInJSON = {'content' => "#{output_file.read}"}
 				outputInJSON.to_json
-			
-			else 
-			
+
+			else
+
 				## Exit with status code 502
 				status 502
 				"Content not successfully converted to braille.".to_json
-				
+
 			end
-			
+
 		ensure
-		
+
 			##Close the files
 			input_file.close
 			output_file.close
-			
+
 			##Unlink the files
 			input_file.unlink
 			output_file.unlink
-			
+
 		end
-	
+
 	else
-	
+
 		##Close the files
 		input_file.close
 		output_file.close
-			
+
 		##Unlink the files
 		input_file.unlink
 		output_file.unlink
-		
+
 		status 400
 		"You must specify content to convert to Braille.".to_json
-	
+
 	end
-		
+
 end
